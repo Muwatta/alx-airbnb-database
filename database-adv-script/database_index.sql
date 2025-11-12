475268cdc799bb4a850871f24d9faed50cfd2887
Muwatta <abdullahmusliudeen@gmail.com>
2025-11-12 05:27:00 +0100

    Add EXPLAIN ANALYZE queries for index performance

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -13,6 +13,14 @@ CREATE INDEX idx_bookings_start_date ON bookings(start_date);
 CREATE INDEX idx_properties_city ON properties(city);
 CREATE INDEX idx_properties_owner_id ON properties(owner_id);
 
--- After creating indexes, use EXPLAIN to check performance improvements
--- Example:
--- EXPLAIN SELECT * FROM bookings WHERE user_id = 1;
+-- Measure query performance using EXPLAIN ANALYZE
+-- Example queries:
+
+-- Check performance before and after index on bookings.user_id
+EXPLAIN ANALYZE SELECT * FROM bookings WHERE user_id = 1;
+
+-- Check performance before and after index on bookings.start_date
+EXPLAIN ANALYZE SELECT * FROM bookings WHERE start_date BETWEEN '2025-01-01' AND '2025-01-31';
+
+-- Check performance before and after index on properties.city
+EXPLAIN ANALYZE SELECT * FROM properties WHERE city = 'Lagos';
