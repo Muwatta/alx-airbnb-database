464e353f3d21b5efd17d8fa0702e9a73c163e515
Muwatta <abdullahmusliudeen@gmail.com>
2025-11-12 05:30:53 +0100

    Add WHERE filter and EXPLAIN ANALYZE for performance optimization

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -7,7 +7,18 @@ SELECT
 FROM bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
-LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+WHERE b.start_date >= '2025-01-01' AND b.end_date <= '2025-12-31';
 
--- After analyzing performance with EXPLAIN:
--- Consider using indexes on user_id, property_id, and booking_id
+-- Analyze the query’s performance
+EXPLAIN ANALYZE
+SELECT 
+    b.booking_id,
+    u.user_id, u.first_name, u.last_name,
+    p.property_id, p.property_name,
+    pay.payment_id, pay.amount, pay.status
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+WHERE b.start_date >= '2025-01-01' AND b.end_date <= '2025-12-31';
